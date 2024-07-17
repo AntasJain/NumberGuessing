a184dbdd25957fd08c00569737884b9174c52b88
Antas Jain <jainantas.99@gmail.com>
2024-07-17 19:06:56 -0400

    Creating Initial Screen
    Implemented:
    - Custom Buttons using Pressables
    - Styled the buttons
    - created Start Screen and Implemented those buttons and an input field
    - Added validation to the input field
    -Added Styling to whole application

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,78 @@
+import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
+
+export default function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState("");
+  function numberInputhandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      //show alert
+      Alert.alert("Invalid Number!", "Number had to be between 1 and 99", [
+        { text: "Okay", style: "destructive", onPress: resetInputHandler },
+      ]);
+      return;
+    }
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        onChangeText={numberInputhandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#3b021f",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
